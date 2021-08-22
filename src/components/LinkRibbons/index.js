59be8306211757a2caa66b4c3168466a5a0b5799
MilosMladenovicWork <milos.mladenovic.work@gmail.com>
2021-08-22 19:03:03 +0200

    fix: link ribbons hash path

--- src/components/LinkRibbons/index.js
+++ src/components/LinkRibbons/index.js
@@ -1,25 +1,30 @@
-import React from 'react'
-import {Link} from 'gatsby'
+import React from "react";
+import { Link } from "gatsby";
 
-import styles from './link-ribbons.module.scss'
-import TiltableContainer from '../TiltableContainer'
+import styles from "./link-ribbons.module.scss";
+import TiltableContainer from "../TiltableContainer";
 
-const LinkRibbons = ({links}) => {
-    return(
-        <ul className={styles.linkRibbonContainer}>
-            {links.map((link, index) => {
-                return <li key={index}>
-                        <TiltableContainer>
-                            <div className={styles.linkStyledContainer}>
-                                <Link to={link.href}>
-                                    {link.text}
-                                </Link>
-                            </div>
-                        </TiltableContainer>
-                    </li>
-            })}
-        </ul>
-    )
-}
+const LinkRibbons = ({ links }) => {
+  return (
+    <ul className={styles.linkRibbonContainer}>
+      {links.map((link, index) => {
+        console.log(link.href);
+        return (
+          <li key={index}>
+            <TiltableContainer>
+              <div className={styles.linkStyledContainer}>
+                <Link
+                  to={link.href && link.href.replace("https://fiducia.ag", "")}
+                >
+                  {link.text}
+                </Link>
+              </div>
+            </TiltableContainer>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
 
-export default LinkRibbons
+export default LinkRibbons;
