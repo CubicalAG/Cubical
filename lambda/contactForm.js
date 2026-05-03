cb8ef4c2fa48a667232778ef9be07b115910233c
Aleksandar-Jocovic-Work <aleksandar@scentronix.com>
2026-05-03 21:06:48 +0200

    fix: :fire: log form err

--- lambda/contactForm.js
+++ lambda/contactForm.js
@@ -1,17 +1,17 @@
-require("dotenv").config();
-const sgMail = require("@sendgrid/mail");
+require('dotenv').config()
+const sgMail = require('@sendgrid/mail')
 
-const { SENDGRID_API_KEY, SENDGRID_TO_EMAIL } = process.env;
+const { SENDGRID_API_KEY, SENDGRID_TO_EMAIL } = process.env
 
-sgMail.setApiKey(SENDGRID_API_KEY);
+sgMail.setApiKey(SENDGRID_API_KEY)
 
 module.exports.handler = async function (event, context) {
-  const data = event.queryStringParameters;
+  const data = event.queryStringParameters
 
   const msg = {
     to: SENDGRID_TO_EMAIL,
     from: SENDGRID_TO_EMAIL,
-    subject: "Fiducia Kontakt",
+    subject: 'Fiducia Kontakt',
     html: `
         <h1 style='color: #f04d23;'>Fiducia Kontakt Form</h1>
             <table style='margin-bottom:15px; border:1px solid #f04d23;border-collapse: collapse'>
@@ -38,26 +38,27 @@ module.exports.handler = async function (event, context) {
             </table>         
         <p style='color: #f04d23;'>Message: ${data.msg}</p>
         `,
-  };
+  }
   try {
-    await sgMail.send(msg);
-    let successBody = JSON.stringify({
-      status: "success",
+    await sgMail.send(msg)
+    const successBody = JSON.stringify({
+      status: 'success',
       message:
-        "Vielen Dank. Wir haben Ihre Anfrage erhalten. Unser Team wird schnellstmöglich innerhalb der nächsten Tagen mit den notwendigen Informationen auf Sie zurückkommen. Für Ihr Verständnis und Ihre Geduld bedanken wir uns im Voraus.",
-    });
+        'Vielen Dank. Wir haben Ihre Anfrage erhalten. Unser Team wird schnellstmöglich innerhalb der nächsten Tagen mit den notwendigen Informationen auf Sie zurückkommen. Für Ihr Verständnis und Ihre Geduld bedanken wir uns im Voraus.',
+    })
     return {
       statusCode: 200,
       body: successBody,
-    };
+    }
   } catch (error) {
-    let errorBody = JSON.stringify({
-      status: "error",
-      message: "There was some error with our servers. Try later!",
-    });
+    console.error(error)
+    const errorBody = JSON.stringify({
+      status: 'error',
+      message: 'There was some error with our servers. Try later!',
+    })
     return {
       statusCode: 500,
       body: errorBody,
-    };
+    }
   }
-};
+}
